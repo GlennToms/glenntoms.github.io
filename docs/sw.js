3d859e8e256e61de7c1dc0a88da70887e5012a18
Glenn Toms <Glenn@glenntoms.gg>
2022-03-02 21:18:45 +0000

    forgot to build

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220302.2109';
+const cacheName = 'chirpy-20220302.2118';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
